db1e6825ab40792d9927e6df79d4cbd78652a7f5
David Salazar <davidksalazar@gmail.com>
2016-03-24 20:54:36 -0500

    Adding priority mail service api call.

---
+++ src/PriorityMailServiceStandards.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace USPS;
+
+/**
+ * Class PriorityMailServiceStandards
+ *
+ * @package USPS
+ */
+class PriorityMailServiceStandards extends USPSBase
+{
+    /**
+     * @var string - the api version used for this type of call
+     */
+    protected $apiVersion = 'PriorityMail';
+    /**
+     * @var array - route added so far.
+     */
+    protected $route = [];
+
+    /**
+     * Perform the API call.
+     *
+     * @return string
+     */
+    public function getServiceStandard()
+    {
+        return $this->doRequest();
+    }
+
+    /**
+     * returns array of all routes added so far.
+     *
+     * @return array
+     */
+    public function getPostFields()
+    {
+        return $this->route;
+    }
+
+    /**
+     * Add route to the stack.
+     *
+     * @param $origin_zip
+     * @param $destination_zip
+     */
+    public function addRoute($origin_zip, $destination_zip)
+    {
+        $this->route = [
+            'OriginZip'      => $origin_zip,
+            'DestinationZip' => $destination_zip,
+        ];
+    }
+}
